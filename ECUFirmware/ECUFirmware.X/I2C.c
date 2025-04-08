ed3431f7ce9ac304e5eb0e982cd3c96d6b8c7fab
cwlebgarcia <calebf3g4@gmail.com>
2025-04-08 17:19:39 -0600

    Created I2C Library Prototype

---
+++ ECUFirmware/ECUFirmware.X/I2C.c
@@ -0,0 +1,63 @@
+/*
+ * File:   I2C.c
+ * Author: Caleb
+ *
+ * Created on April 8, 2025,
+ */
+
+#include <avr/io.h>
+
+#include "I2C.h"
+
+
+
+void I2CInitialization(){
+    if (I2C_MASTER)
+    {
+        TWI0.MBAUD() =  0b00001111; //  Standard mode is up to 100kHz
+                                    //  Need to figure out what our main clock is going to
+                                    //  run at. I think we're at the default 4MHz
+        
+        PORTMUX.TWIROUTEA() = 0b00000000; // Selects  PA2 and PA3 as Serial Data (SDA) and Serial Clock (SCL), respectively
+        TWI0.CTRLA() = 0b00000000;        // Initialize hold times and shit
+        
+        TWI0.MCTRLA() = 0b11000011;       // Enable read and write interrupts, enables
+                                          // smart mode, and configures the MCU as
+                                          // TWI master.
+        
+        TWI0.MCTRLB() = 0b00000100; // Enables Smart mode auto-acknowledge.
+        
+        TWI0.MSTATUS() = 0b00000001; // Forces the bus state to idle.
+    }
+    
+    else
+    {
+        TWI0.SCRTLA() = 0b00000011; // Register used for interrupts, although I
+                                    // don't have these enabled right now.
+                                    // Currently this enables Smart mode and
+                                    // enables the device as the TWI slave.
+        
+        TWI0.SADDR() = I2C_MTR_SLV_ADDR + 1; // This is a dummy address for now.
+                                             // Change this macro to reflect which
+                                             // device is acting as a slave. Demonic
+                                             // addition trick to use the same macro
+                                             // to configure master and slave side
+                                             // operations.
+        
+    }
+}
+
+// Remains to be seen how these work with the Smart mode auto acknowledge.
+// In theory, most of the code here should be handled by Smart mode, but this 
+// looks way to easy.
+
+void I2CMasterTransfer(char slaveAddr, char data, int dataLength) {  
+    TWI0.MDATA() = data;
+    TWI0.MADDR() = slaveAddr + 1;
+    TWI0.MCTRLB() = 
+    ACK_STALL;
+}
+
+char I2CSlaveRecieve(char *data) {
+    data = TWI0.SDATA();
+}
---
+++ ECUFirmware/ECUFirmware.X/I2C.h
@@ -0,0 +1,53 @@
+/* 
+ * File: radioSPI.h
+ * Author: Vikram Procter and Caleb Garcia
+ * Comments: ENEL 300 Didsbury Diddlers & Co, est. 2006
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_RADIO_SPI_H
+#define	XC_RADIO_SPI_H
+
+#include <xc.h> // include processor files - each processor file is guarded.
+
+#define I2C_MASTER 1
+
+// Least significant bit of these values is set to one so that it is possible
+// to add one for configuration purposes.
+
+#define I2C_MTR_SLV_ADDR 0b11111110
+#define I2C_SNSR_SLV_ADDR 0b01111110
+#define I2C_SCRN_SLV_ADDR 0b00111110
+
+#define ACK_STALL while(!(TWI0.MSTATUS() & 0b0001000)) // Valid for both the master and slave status registers
+#define MASTER_STOP TWI0.MCTRLB() = 0b00000111 // Ends master transmission
+                                                       
+
+// #include <>
+
+
+//  Insert declarations
+void I2CInitialization(); // Initializes TWI depending on the value of the 
+                          // I2C_MASTER macro; whether the board is set as a
+                          // master or a slave.
+
+// As of now these definitions are slightly shoddy as they necessitate the
+// creation of both master and slave side transfer and receive commands.
+// We might be able to fix this issue with the use of function pointers or 
+// function overloading.
+
+void I2CMasterTransfer(char *data, int dataLength); // Initiates a data transfer.
+                                                    // Must decide on data length.
+void I2CMasterReceieve(char *data); // Receives and writes data to a local variable
+
+void I2CSlaveTransfer(char *data, int dataLength); // Initiates a data transfer.
+                                                    // Must decide on data length
+
+void I2CSlaveRecieve(char *data); // Receives and writes data to a local variable
+
+
+
+#endif	/* XC_RADIO_SPI_H */
+
