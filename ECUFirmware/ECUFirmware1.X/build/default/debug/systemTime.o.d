00bc3a19147ebc29dc6d05721dd7e621598ab41a
cwlebgarcia <calebf3g4@gmail.com>
2025-04-08 01:14:33 -0600

    W

---
+++ ECUFirmware/ECUFirmware1.X/build/default/debug/systemTime.o.d
@@ -0,0 +1,47 @@
+build/default/debug/systemTime.o.d build/default/debug/systemTime.o: \
+ systemTime.c \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\io.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\sfr_defs.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\inttypes.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\features.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\stdint.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\bits\alltypes.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\bits\stdint.h \
+ C:/Program\ Files/Microchip/MPLABX/v6.20/packs/Microchip/AVR-Dx_DFP/2.4.286/xc8/avr/include/avr/ioavr128db28.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\portpins.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\common.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\xmega.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\fuse.h \
+ c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\lock.h \
+ systemTime.h
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\io.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\inttypes.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\features.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\bits\alltypes.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\bits\stdint.h:
+
+C:/Program\ Files/Microchip/MPLABX/v6.20/packs/Microchip/AVR-Dx_DFP/2.4.286/xc8/avr/include/avr/ioavr128db28.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\portpins.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\common.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\xmega.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\fuse.h:
+
+c:\program\ files\microchip\xc8\v3.00\avr\avr\include\avr\lock.h:
+
+systemTime.h:
---
+++ ECUFirmware/ECUFirmware1.X/systemTime.c
@@ -0,0 +1,13 @@
+/*
+ * File:   systemTime.c
+ * Author: vikra
+ *
+ * Created on April 3, 2025, 10:53 AM
+ */
+
+
+#include <avr/io.h>
+
+#include "systemTime.h"
+
+
